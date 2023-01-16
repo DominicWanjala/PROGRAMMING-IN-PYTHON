fcf4d30686adc98d4c2f8657acb81eac578badb4
NatashaPatricia <122792084+NatashaPatricia@users.noreply.github.com>
2023-01-16 16:41:13 +0300

    Add files via upload

---
+++ Hhello.py
@@ -0,0 +1,15 @@
+# programming in python
+'''
+author:talia
+publisher: lomghorn
+year:9876
+'''
+print("Hello World")
+#boolen
+print(True)
+#float
+a=456.6788
+print(a)
+#string
+name='python'
+print (name)
